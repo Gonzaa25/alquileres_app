9ff87d1291e007b027a92f9c8642c271a0527362
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-02 11:24:13 -0300

    favourite page

---
+++ lib/pages/favourites.dart
@@ -0,0 +1,56 @@
+import 'package:alquileres_app/providers/favsprovider.dart';
+import 'package:alquileres_app/widgets/homecard.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FavouritePage extends StatefulWidget {
+  FavouritePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FavouritePageState createState() => _FavouritePageState();
+}
+
+class _FavouritePageState extends State<FavouritePage> {
+  @override
+  Widget build(BuildContext context) {
+    final casas = context.watch<FavsProvider>().myfavs;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoritos'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            //LIST VIEW
+            return ListView(
+              itemExtent: 330,
+              padding: EdgeInsets.all(10),
+              children: [
+                for (var casa in casas)
+                  HomeCard(
+                    datos: casa,
+                  )
+              ],
+            );
+          } else {
+            //GRID VIEW
+            return GridView(
+                padding: EdgeInsets.all(10),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+                children: [
+                  for (var casa in casas)
+                    HomeCard(
+                      datos: casa,
+                    )
+                ]);
+          }
+        },
+      ),
+    );
+  }
+}
