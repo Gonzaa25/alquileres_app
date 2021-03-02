99934c3cc87096ee502ef4e6df5d95456567b935
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-02 10:39:21 -0300

    adding provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:alquileres_app/pages/home.dart';
+import 'package:alquileres_app/providers/favsprovider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => FavsProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage(title: 'Alquiler Directo'),
       ),
-      home: HomePage(title: 'Alquiler Directo'),
     );
   }
 }
